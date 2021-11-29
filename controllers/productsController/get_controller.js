649de9522f0215ffe56509a53de1980a0f30b37b
LoopyPanda <80119515+LoopyPanda@users.noreply.github.com>
2021-11-29 15:22:37 +0100

    add instapicture data

--- controllers/productsController/get_controller.js
+++ controllers/productsController/get_controller.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const db = require("../../db/client");
+const abhiInstaData = require("../../db/abhi_insta_data.json");
 
 const get_one_product_by_id = async (req, res, next) => {
 const { id } = req.params;
@@ -55,6 +56,39 @@ try {
 }
 };
 
+const get_insta_pictures = async (req, res, next) => {
+    const getInstaData = async () => {
+        const {
+          data: {
+            user: {
+              edge_owner_to_timeline_media: { edges },
+            },
+          },
+        } = abhiInstaData;
+      
+        const data = edges
+          .filter(({ node }) => !node.is_video)
+          .map(({ node }) => {
+            return {
+              bigPicture: node.display_url,
+              smallPicture: node.display_resources?.[0]?.src,
+            };
+          });
+      
+        //   const queryHash = "8c2a529969ee035a5063f2fc8602a0fd";
+        //   const variables =
+        //     "%7B%22id%22%3A%226356671502%22%2C%22first%22%3A12%2C%22after%22%3A%22QVFEVURuN1pQN0JMWWdFbjRNVWRRZnVTdURlVHdnd0xON2NXZGN5NjU2bkpjWnlLRDVaOGlyRkVJcGVlRDVlU01VOXhiUzFWOG5yd2tlcFgxOWpWWE5fcw%3D%3D%22%7D";
+        //   const data = await axios.get(
+        //     `https://www.instagram.com/graphql/query/?query_hash=${queryHash}`
+        //   );
+      
+        return data;
+      };
+      const data = await getInstaData();
+      res.json(data)
+}
+
 module.exports = {
 get_one_product_by_id,
+get_insta_pictures
 };
